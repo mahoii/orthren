0aa9d61cc511fd715ec5dd1c8d5f416ce6eaedd3
kamari <kamarishabazz@gmail.com>
2026-06-27 23:44:36 -0400

    token burn leak fixed

--- greenlitmd-main/next.config.mjs
+++ greenlitmd-main/next.config.mjs
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverActions: {
-    allowedOrigins: [
-      "greenlitmd.app",
-      "www.greenlitmd.app",
-      "orthren.com",
-      "www.orthren.com",
-      "*.vercel.app" // Allows Vercel preview branch deployments to work
-    ]
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "greenlitmd.app",
+        "www.greenlitmd.app",
+        "orthren.com",
+        "www.orthren.com",
+        "*.vercel.app" // Allows Vercel preview branch deployments to work
+      ]
+    }
   }
 };
 
